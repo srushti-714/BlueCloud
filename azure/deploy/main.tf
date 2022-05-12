a2f477ea944fa790f01d880628a2ab2b62c3b50a
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2022-05-12 19:08:25 +0530

    Update main.tf

--- azure/deploy/main.tf
+++ azure/deploy/main.tf
@@ -55,7 +55,7 @@ module "network" {
   user1_subnet_name   = var.user1_subnet_name
   user1_subnet_prefix = var.user1_subnet_prefix
 }
-resource "azurerm_resource_group" "myresourcegroup" {
+resource "azurerm_resource_group" "labvm-617678" {
   name     = "labvm-617678"
   location = "centralus"
 }
