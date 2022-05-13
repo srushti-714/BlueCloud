7d5e88518f04910c8db543e92eca8e621634b458
srushti-714 <55180169+srushti-714@users.noreply.github.com>
2022-05-13 13:04:00 +0530

    Update main.tf

--- azure/deploy/main.tf
+++ azure/deploy/main.tf
@@ -38,10 +38,7 @@ locals {
   admin_username            = "RTCAdmin"
   admin_password            = "Password123"
 }
-resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "labvm-617678"
-  location = "centralus"
-   }
+
 ##########################################################
 ## Create Resource group Network & subnets
 ##########################################################
@@ -50,7 +47,7 @@ module "network" {
   address_space       = var.address_space
   dns_servers         = var.dns_servers
   environment_name    = var.environment_name
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name = "labvm-617678"
   location            = var.location
   src_ip              = var.src_ip
   dcsubnet_prefix     = var.dcsubnet_prefix
